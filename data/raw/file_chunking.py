70620f2f597f0a202ad1a9ab9c8a5d46ac8574ee
VohraAK <abdullah.k.vohra@gmail.com>
2025-07-06 15:47:57 +0500

    initialised DVC

--- data/raw/file_chunking.py
+++ data/raw/file_chunking.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 from config import CHUNK_SIZE
 
-chunks = pd.read_json("./arxiv-metadata-oai-snapshot.json", chunksize=CHUNK_SIZE, lines=True)
+chunks = pd.read_json("./data/raw/arxiv-metadata-oai-snapshot.json", chunksize=CHUNK_SIZE, lines=True)
 
 for i, chunk in enumerate(chunks):
     # save each chunk as a CSV file
-    save = chunk.to_csv(f"arXiv_{i+1}.csv")
+    save = chunk.to_csv(f"./data/raw/arXiv_{i+1}.csv")
